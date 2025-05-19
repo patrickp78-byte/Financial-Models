22ab09f2faf8ee7b24a67ac9bea7eba5ee9008e6
Patrick Pascua <patrick.pascua78@gmail.com>
2025-05-19 08:31:17 -0600

    Garch Added and library added

--- modelling.py
+++ modelling.py
@@ -14,24 +14,51 @@ import os
 import scipy as sc
 import arch
 
-
 file_loc = os.path.abspath(__file__)
 dir_path = os.path.dirname(file_loc)
 os.chdir(dir_path)
 print(os.getcwd())
 
-url = "G://GitHub//Financial-Models//Back Up Data//full_data_back_up_2025-05-08.pkl"
+url = "full_data.pkl"
 
 with open(url, 'rb') as r:
     loaded_data = pickle.load(r)
 
-d_query = loaded_data["XCD.TO"]["Close"]
-d_query = d_query.loc[dt.date(2020,1,1):]
+d_query = loaded_data["^GSPC"]["Close"]
+d_query = d_query.loc[dt.date(2010,1,1):]
 d_query = d_query.reset_index().drop(columns= "Date")
 d_query = d_query.pct_change().dropna().reset_index().drop(columns = "index")
 
+# This function creates a graph from a list of a tuples
+def graphing(data, t):
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots() 
+    for i in data:
+        ax.plot(i[0], label= f'{i[1]}')  
+    plt.legend()
+    plt.title(t)
+    plt.show()
 
 
+
+# This function uses the arch_library to calculate the model
+def arch_lib(data,mean, vol, p,o,q, res ):
+    res = res.upper()
+    lib_garch = arch.arch_model(data, mean =mean, vol = vol , p= p , o = o, q = q, rescale = False)
+    model_garch = lib_garch.fit()
+    lib_garch= model_garch.params
+    libr_result = pd.DataFrame()
+    libr_result['lib_resid'] = model_garch.resid
+    libr_result['lib_sqr_resid'] = libr_result['lib_resid']**2
+    libr_result['lib_condvol'] = model_garch.conditional_volatility
+    libr_result['lib_condvar'] = libr_result['lib_condvol']**2
+    if res == 'PARAMS':
+        return lib_garch
+    elif res == 'DF':
+        return libr_result
+    else:
+        return []
+    
 #%%
 ##### ARCH
 
@@ -52,7 +79,7 @@ def arch_model(params,data):
     resid = data - mu
     sqresid = resid **2
     cond_var= np.zeros(len(sqresid))
-    cond_var[0] = l_run_vol
+    cond_var[0] = l_run_vol**2
     
     for i in range(1,len(resid)):
         cond_var[i] = omega + alpha * sqresid[i-1].item()
@@ -60,8 +87,8 @@ def arch_model(params,data):
     l_likelihood = np.zeros(len(cond_var))   
     for i in range(len(cond_var)):   
         l_likelihood[i] = (-1/2) * (np.log(2*np.pi) + np.log(cond_var[i].item()) + (sqresid[i].item()/cond_var[i].item()))
-    l_like = -np.sum(l_likelihood)   
-    return l_like
+    l_like = np.sum(l_likelihood)   
+    return -l_like
 
 
 stdev = float(d_query.std().iloc[0]) 
@@ -75,8 +102,10 @@ param = [un_var,0.5]
 bounds = [(1e-6, None), (1e-6, 1 - 1e-6)]
 
 constraints = [
+    {'type': 'ineq', 'fun': lambda x: x[1]},       # alpha ≥ 0
     {'type': 'ineq', 'fun': lambda x: 1 - x[1]},  # alpha < 1
     {'type': 'ineq', 'fun': lambda x: x[0]}       # omega ≥ 0
+    
 ]
 
 ### Good MLE method 
@@ -92,3 +121,80 @@ mle_param_res = pd.DataFrame([mle_omega,mle_alpha], index = ["mle_omega","mle_al
 
 
 
+#%%
+
+#GARCH model
+param = arch_lib(d_query, 'Zero', 'GARCH', 1,0,1,'PARAMS' )
+df = arch_lib(d_query, 'Zero', 'GARCH', 1,0,1,'df' )
+
+def garch(params, data, res):
+    print(params)
+    res = res.upper()
+    data = np.array(data)
+    mu, om, alpha,beta = 0, params[0], params[1],params[2]
+    # long_run = (om/(1- alpha - beta))**0.5
+    
+    resid = data-mu
+    sq_resid = resid**2
+    
+    cond_var = np.zeros(len(resid))
+    cond_var[0]= om/(1- alpha - beta)
+    
+    for i in range(1,len(resid)):
+        cond_var[i] = om + alpha * sq_resid[i-1].item() + beta* cond_var[i-1].item()
+    
+    log_likelihood = np.zeros(len(cond_var))
+    
+    for i in range(len(log_likelihood)):
+        log_likelihood[i] = (-1/2) * (np.log(2*np.pi) + np.log(cond_var[i].item()) + (sq_resid[i].item()/cond_var[i].item()))
+    
+    sum_l_like = np.sum(log_likelihood)
+    
+    if res == 'CONDITIONAL_VAR':
+        return cond_var
+    elif res == 'CONDITIONAL_VOL':
+        return cond_var**0.5
+    elif res == 'RESID':
+        return cond_var**0.5
+    elif res == 'SQR_RESID':
+        return cond_var**0.5
+    elif res == 'L_LIKE':
+        return -sum_l_like
+    else:
+        return []
+
+stdev = float(d_query.std().iloc[0]) 
+var= stdev**2
+un_var = var
+
+params = [0,0.5,0.5]
+bounds = [(1e-10, None), (1e-6, 1 - 1e-6), (1e-6, 1 - 1e-6)]
+
+constraints = [
+    {'type': 'ineq', 'fun': lambda x: x[1]},       # omega ≥ 0
+    {'type': 'ineq', 'fun': lambda x: x[2]},       # alpha ≥ 0
+    {'type': 'ineq', 'fun': lambda x: x[3]},       # beta ≥ 0
+    {'type': 'ineq', 'fun': lambda x: 1 - x[2]},  # alpha < 1
+    {'type': 'ineq', 'fun': lambda x: 1 - x[3]},  # beta < 1
+    {'type': 'ineq', 'fun': lambda x: 1- x[2]-x[3]  } # ensures stationarity
+]
+        
+# Uses MLE From Scipy
+maximize = sc.optimize.minimize(fun =garch , x0=param, args=(d_query,'L_LIKE'),\
+                                method= "L-BFGS-B" , bounds = bounds)
+
+# These are the Parameter results from MLE
+mle_omega,mle_alpha,mle_beta = maximize.x
+mle_param_res = pd.DataFrame([mle_omega,mle_alpha,mle_beta], index = ["mle_omega","mle_alpha","mle_beta"])
+
+# Graphs the results
+result_mle = garch([mle_omega,mle_alpha,mle_beta], d_query, 'CONDITIONAL_Vol')
+graph = [((d_query**2)**(1/2), 'actual vol'),(result_mle,'forcast'),(df['lib_condvol'], 'lib result')]
+graphing(graph, "Conditional vol")
+
+#%%
+
+#EGARCH Model
+
+    
+    
